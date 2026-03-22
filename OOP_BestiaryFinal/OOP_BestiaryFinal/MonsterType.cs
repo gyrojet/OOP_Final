90199d89eb71a8bcb12495d567df87c93b36c5a5
Tyler Law <Shinnok1122@gmail.com>
2026-03-22 19:46:22 -0300

    Begining Work

---
+++ OOP_BestiaryFinal/OOP_BestiaryFinal/MonsterType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_BestiaryFinal
+{
+    public enum MonsterType
+    {
+        Animal,
+        Alien,
+        Construct,
+        Demon,
+        Dragon,
+        Humanoid,
+        Ooze,
+        Undead
+    }
+}
